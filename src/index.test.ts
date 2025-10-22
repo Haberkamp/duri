504ee4b557cf17b70fec41fb96f46e39b46620a1
Nils Haberkamp <haberkamp.n@gmail.com>
2025-10-22 22:24:42 +0100

    test: group tests in describe blocks

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,91 +1,101 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { Duration } from "./index.js";
 
-it("creates a duration of 1234 milliseconds via method", () => {
-  // ARRANGE
-  const subject = Duration;
+describe("create duration from methods", () => {
+  it("creates a duration of 1234 milliseconds via method", () => {
+    // ARRANGE
+    const subject = Duration;
 
-  // ACT
-  const duration = subject.milliseconds(1234);
+    // ACT
+    const duration = subject.milliseconds(1234);
 
-  // ASSERT
-  expect(duration.toSeconds()).toBe(1.234);
+    // ASSERT
+    expect(duration.toSeconds()).toBe(1.234);
+  });
+
+  it("creates a duration of 5 seconds via method", () => {
+    // ARRANGE
+    const subject = Duration;
+
+    // ACT
+    const duration = subject.seconds(5);
+
+    // ASSERT
+    expect(duration.toSeconds()).toBe(5);
+  });
+
+  it("creates a duration of 1 minute via method", () => {
+    // ARRANGE
+    const subject = Duration;
+
+    // ACT
+    const duration = subject.minutes(1);
+
+    // ASSERT
+    expect(duration.toSeconds()).toBe(60);
+  });
+
+  it("creates a duration of 1 hour via method", () => {
+    // ARRANGE
+    const subject = Duration;
+
+    // ACT
+    const duration = subject.hours(1);
+
+    // ASSERT
+    expect(duration.toSeconds()).toBe(3600);
+  });
 });
 
-it("creates a duration of 5 seconds via method", () => {
-  // ARRANGE
-  const subject = Duration;
+describe("convert duration into milliseconds", () => {
+  it("converts duration into milliseconds", () => {
+    // ARRANGE
+    const subject = Duration;
 
-  // ACT
-  const duration = subject.seconds(5);
+    // ACT
+    const duration = subject.seconds(5);
 
-  // ASSERT
-  expect(duration.toSeconds()).toBe(5);
+    // ASSERT
+    expect(duration.toMilliseconds()).toBe(5000);
+  });
 });
 
-it("creates a duration of 1 minute via method", () => {
-  // ARRANGE
-  const subject = Duration;
+describe("convert duration into seconds", () => {
+  it("converts duration to seconds", () => {
+    // ARRANGE
+    const subject = Duration;
 
-  // ACT
-  const duration = subject.minutes(1);
+    // ACT
+    const duration = subject.seconds(5);
 
-  // ASSERT
-  expect(duration.toSeconds()).toBe(60);
+    // ASSERT
+    expect(duration.toSeconds()).toBe(5);
+  });
 });
 
-it("creates a duration of 1 hour via method", () => {
-  // ARRANGE
-  const subject = Duration;
+describe("convert duration into minutes", () => {
+  it("converts duration into minutes", () => {
+    // ARRANGE
+    const subject = Duration;
 
-  // ACT
-  const duration = subject.hours(1);
+    // ACT
+    const duration = subject.seconds(5);
 
-  // ASSERT
-  expect(duration.toSeconds()).toBe(3600);
+    // ASSERT
+    expect(duration.toMinutes()).toBe(0.083_333_333_333_333_33);
+  });
 });
 
-it("converts duration to seconds", () => {
-  // ARRANGE
-  const subject = Duration;
+describe("convert duration into hours", () => {
+  it("converts duration into hours", () => {
+    // ARRANGE
+    const subject = Duration;
 
-  // ACT
-  const duration = subject.seconds(5);
+    // ACT
+    const duration = subject.seconds(3600);
 
-  // ASSERT
-  expect(duration.toSeconds()).toBe(5);
-});
-
-it("converts duration into milliseconds", () => {
-  // ARRANGE
-  const subject = Duration;
-
-  // ACT
-  const duration = subject.seconds(5);
-
-  // ASSERT
-  expect(duration.toMilliseconds()).toBe(5000);
-});
-
-it("converts duration into minutes", () => {
-  // ARRANGE
-  const subject = Duration;
-
-  // ACT
-  const duration = subject.seconds(5);
-
-  // ASSERT
-  expect(duration.toMinutes()).toBe(0.083_333_333_333_333_33);
-});
-
-it("converts duration into hours", () => {
-  // ARRANGE
-  const subject = Duration;
-
-  // ACT
-  const duration = subject.seconds(3600);
-
-  // ASSERT
-  expect(duration.toHours()).toBe(1);
+    // ASSERT
+    expect(duration.toHours()).toBe(1);
+  });
 });
